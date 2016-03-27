592a2a3254361140a855d965d3394e76c89522a5
Laco Papay <lacop@lacop.net>
2016-03-27 22:46:28 +0200

    More experimental data

--- sha3_experiments.py
+++ sha3_experiments.py
@@ -53,7 +53,7 @@ def experiment_1():
     # SHA3, message length = 32, preimage = 8bit reference
     # Different number of rounds
     # Test with different branch orders
-    # Multiple runs each (10x10=100 total)
+    # Multiple runs each (10 different seeds x 10 tries = 100 total)
     # Timeout = 60s
 
     branch_orders = {
@@ -74,10 +74,47 @@ def experiment_1():
                     ['timeout', '60', './minisatrun.sh'],
                     bo, bodesc,
                     10)
-experiment_1()
+#experiment_1()
 
-#multiple_runs('experiments/tmp.csv',
-#              32, 24, 8,
-#              ['timeout', '30', './minisatrun.sh'],
-#              genbo([lambda rv: rv[0][0][x][y] for x in range(5) for y in range(5)]), 'r0-S-x-y',
-#              1)
+def experiment_2():
+    # SHA3, message length = 32, preimage = 4bit reference
+    # Different number of rounds
+    # Test with different branch orders (more than ex1)
+    # Multiple runs each (5 different seeds x 2 tries = 10 total)
+    # Timeout = 60s
+
+    branch_orders = {
+        'none': None,
+        'r0-S-x-y': genbo([lambda rv: rv[0][0][x][y] for x in range(5) for y in range(5)]),
+        'r0-S-y-x': genbo([lambda rv: rv[0][0][x][y] for y in range(5) for x in range(5)]),
+        'r1-S-x-y': genbo([lambda rv: rv[1][0][x][y] for x in range(5) for y in range(5)]),
+        'r1-S-y-x': genbo([lambda rv: rv[1][0][x][y] for y in range(5) for x in range(5)]),
+        'rlast-S-x-y': genbo([lambda rv: rv[-1][0][x][y] for x in range(5) for y in range(5)]),
+        'rlast-S-y-x': genbo([lambda rv: rv[-1][0][x][y] for y in range(5) for x in range(5)]),
+        'r0-B-x-y': genbo([lambda rv: rv[0][1][x][y] for x in range(5) for y in range(5)]),
+        'r0-B-y-x': genbo([lambda rv: rv[0][1][x][y] for y in range(5) for x in range(5)]),
+        'r0-B-x-y': genbo([lambda rv: rv[0][1][x][y] for x in range(5) for y in range(5)]),
+        'r0-B-y-x': genbo([lambda rv: rv[0][1][x][y] for y in range(5) for x in range(5)]),
+        'r0-C-x': genbo([lambda rv: rv[0][2][x] for x in range(5)]),
+        'r0-D-x': genbo([lambda rv: rv[0][3][x] for x in range(5)]),
+    }
+
+    for _ in range(2):
+        for rounds in range(2, 25): # Start from 2
+            for bodesc, bo in branch_orders.items():
+                print('Rounds=', rounds, 'BO=', bodesc)
+                multiple_runs(
+                    'experiments/ex2-sha3-minisat-bos.csv',
+                    32, rounds, 8,
+                    ['timeout', '60', './minisatrun.sh'],
+                    bo, bodesc,
+                    5)
+experiment_2()
+
+# multiple_runs('experiments/tmp.csv',
+#               32, 24, 4,
+#               ['timeout', '30', './minisatrun.sh'],
+#               #genbo([lambda rv: rv[0][0][x][y] for x in range(5) for y in range(5)]), 'r0-S-x-y',
+#               genbo([lambda rv: rv[1][0][x][y] for x in range(5) for y in range(5)]), 'r1-S-x-y',
+#               #None, 'none',
+#               1)
